d7aeeb752ec2e2785cb399d2ec665cdc6be164b1
Rodrigogarciactg <100664773+Rodrigogarciactg@users.noreply.github.com>
2022-04-06 11:58:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1.
+// Tesla, Mercedes
+
+// 2.
+// name = not defined other name = Elon
+
+// 3.
+// password = 12345 hashedPassword = undefined
+
+// 4.
+//  2==5 false  2==2 true
+
+// 5.
+// key = value secondKey = [1,5,1,8,3,3] secondKey[0]=1 willThisWork =5
